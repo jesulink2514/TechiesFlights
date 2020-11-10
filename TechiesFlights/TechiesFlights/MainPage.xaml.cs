276e0dc9fa0cf8e62d2c0f1bbbe4e0bb02c08e44
jesulink2514 <jesus.angulo@outlook.com>
2020-11-09 23:48:14 -0500

    Finish

--- TechiesFlights/TechiesFlights/MainPage.xaml.cs
+++ TechiesFlights/TechiesFlights/MainPage.xaml.cs
@@ -17,6 +17,12 @@ namespace TechiesFlights
             InitializeComponent();
         }
 
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            page_height = Height;
+        }
+
         private void HideExplore()
         {            
             ExplorePane.HeightRequest = 0;
@@ -30,6 +36,48 @@ namespace TechiesFlights
             ExplorePane.IsVisible = true;
             ExplorePane.HeightRequest = 170;
             ExplorePane.FadeTo(1);
+        }
+
+        private double oldY = 300;
+        private const double smoothFactor = 0.25;
+
+        private const double upperPoint = 450;
+        private const double lowerPoint = 300;
+
+        private double page_height = 0;
+
+        private void PanGestureRecognizer_OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            Debug.WriteLine($"Total Y: {e.TotalY}");
+
+            var y = e.TotalY;
+
+            y = smoothFactor * (y - oldY);
+
+            var height = InfoPane.HeightRequest + -1 * y;
+
+            height = Math.Max(Math.Min(height, page_height), lowerPoint);
+
+            InfoPane.HeightRequest = height;
+
+            oldY = y;
+
+            if (e.StatusType == GestureStatus.Completed || e.StatusType == GestureStatus.Canceled)
+            {
+                var toUpper = Math.Abs(upperPoint - height);
+                var toBottom = Math.Abs(lowerPoint - height);
+
+                if (toUpper < toBottom)
+                {
+                    InfoPane.HeightRequest = upperPoint;
+                    ShowExplore();
+                }
+                else
+                {
+                    InfoPane.HeightRequest = lowerPoint;
+                    HideExplore();
+                }
+            }
         }
     }
 }
